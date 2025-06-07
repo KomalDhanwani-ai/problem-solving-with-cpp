cc6b861b659f9e01611cc8cf9ab53226b26156b4
KomalDhanwani-ai <komaldhanwani2@gmail.com>
2025-06-07 23:33:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.56%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.cpp
+++ 319-bulb-switcher/bulb-switcher.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-      return sqrt(n);  
+      int count = 0;
+      int i = 1;
+      while(i*i<=n)
+      {
+        count++;
+        i++;
+      }
+    return count;
     }
 };
