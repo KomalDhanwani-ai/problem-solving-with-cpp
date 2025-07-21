bca89427ba39729736d26f878844f5e0e9805722
KomalDhanwani-ai <komaldhanwani2@gmail.com>
2025-07-22 00:57:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (44.23%) - LeetSync

---
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string toHex(int num) {
+        if (num == 0) return "0";
+        
+        unsigned int n = num; // To handle negative numbers
+        string hexChars = "0123456789abcdef";
+        string result = "";
+
+        while (n > 0) {
+            result = hexChars[n % 16] + result;
+            n /= 16;
+        }
+        return result;
+    }
+};
