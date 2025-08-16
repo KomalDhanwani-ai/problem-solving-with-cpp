4cd28418397cb2b4da2f74745035dcce27008396
KomalDhanwani-ai <komaldhanwani2@gmail.com>
2025-08-16 22:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (94.22%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -3,7 +3,8 @@ public:
     int maximum69Number (int num) {
      std::string s = std::to_string(num);
     for (char &c : s) {
-        if (c == '6') { c = '9'; break; }
+        if (c == '6') 
+        { c = '9'; break; }
     }
     return std::stoi(s);
 }
