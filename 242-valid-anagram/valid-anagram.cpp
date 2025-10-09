d9fd68e1e3ed551f008cd27e2c7752bc58125fc6
KomalDhanwani-ai <komaldhanwani2@gmail.com>
2025-10-09 11:47:09 +0530

    Time: 3 ms (51.29%) | Memory: 9.9 MB (26.04%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      map<char,int>m1;
+      map<char,int>m2;
+
+     for(auto x:s){
+        m1[x]++;
+     }
+     for(auto x:t){
+        m2[x]++;
+     }
+     return m1==m2;
+    }
+};
